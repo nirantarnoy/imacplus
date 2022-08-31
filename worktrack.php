eaa6aeec6c3d2c6236f577abbb608dd441d189e5
nirantarnoy <niran_tarlek@hotmail.com>
2022-08-31 20:45:01 +0700

    update workorder

--- worktrack.php
+++ worktrack.php
@@ -111,46 +111,46 @@ $workorder_data = getWorkorder($connect,$id);
     </ul>
 
 
-    <div class="px-2 py-2 mb-4">
-        <div id="step-1">
-          <div class="row">
-
-              <div class="col-lg-12">
-                  <table style="border: 0px;width: 100%">
-                      <tr>
-                          <td style="width: 20%"></td>
-                          <td style="width: 20%;background-color: lightgrey;padding: 10px;">07/07/2022 18:26</td>
-                          <td style="width: 40%;background-color: lightgrey;padding: 10px;">รับเครื่อง Drop off</td>
-                          <td style="width: 20%"></td>
-                      </tr>
-                      <tr>
-                          <td style="width: 20%"></td>
-                          <td style="width: 20%;background-color: lightgrey;padding: 10px;">07/07/2022 18:50</td>
-                          <td style="width: 40%;background-color: lightgrey;padding: 10px;">ตรวจสอบพร้อมส่งต่อ Center</td>
-                          <td style="width: 20%"></td>
-                      </tr>
-                  </table>
-              </div>
-          </div>
-        </div>
-
-
-        <div id="step-2">
-
-        </div>
-
-
-        <div id="step-3" class="text-center">
-            <h3 class="font-light text-primary my-4">
-                Select Payment Method
-            </h3>
-        </div>
-
-
-        <div id="step-4" class="text-center">
-
-        </div>
-    </div>
+<!--    <div class="px-2 py-2 mb-4">-->
+<!--        <div id="step-1">-->
+<!--          <div class="row">-->
+<!---->
+<!--              <div class="col-lg-12">-->
+<!--                  <table style="border: 0px;width: 100%">-->
+<!--                      <tr>-->
+<!--                          <td style="width: 20%"></td>-->
+<!--                          <td style="width: 20%;background-color: lightgrey;padding: 10px;">07/07/2022 18:26</td>-->
+<!--                          <td style="width: 40%;background-color: lightgrey;padding: 10px;">รับเครื่อง Drop off</td>-->
+<!--                          <td style="width: 20%"></td>-->
+<!--                      </tr>-->
+<!--                      <tr>-->
+<!--                          <td style="width: 20%"></td>-->
+<!--                          <td style="width: 20%;background-color: lightgrey;padding: 10px;">07/07/2022 18:50</td>-->
+<!--                          <td style="width: 40%;background-color: lightgrey;padding: 10px;">ตรวจสอบพร้อมส่งต่อ Center</td>-->
+<!--                          <td style="width: 20%"></td>-->
+<!--                      </tr>-->
+<!--                  </table>-->
+<!--              </div>-->
+<!--          </div>-->
+<!--        </div>-->
+<!---->
+<!---->
+<!--        <div id="step-2">-->
+<!---->
+<!--        </div>-->
+<!---->
+<!---->
+<!--        <div id="step-3" class="text-center">-->
+<!--            <h3 class="font-light text-primary my-4">-->
+<!--                Select Payment Method-->
+<!--            </h3>-->
+<!--        </div>-->
+<!---->
+<!---->
+<!--        <div id="step-4" class="text-center">-->
+<!---->
+<!--        </div>-->
+<!--    </div>-->
 </div>
 <?php
 include "footer.php";
